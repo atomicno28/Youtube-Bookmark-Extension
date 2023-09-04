05541f36174412cf41813a580a2034b3a3ffe738
Nikhil Patel <72176039+atomicno28@users.noreply.github.com>
2023-09-04 20:21:57 +0530

    Service-worker fucntionality

--- background.js
+++ background.js
@@ -1,12 +1,14 @@
-// on refresh it will load
+// Adding Listener -  On Opening/Refreshing the tabs, we'll check whether tha page is of Extension's matching or not.
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
   if (tab.url && tab.url.includes("youtube.com/watch")) {
 
-    // holds unique key
+    // Specifies the Unique ID for each video.
     const queryParameters = tab.url.split("?")[1];
+
+    // Interface to work with URL search Paramters.
     const urlParameters = new URLSearchParams(queryParameters);
 
-    // sends message to the ContentScript
+    // Sends message to the ContentScript.js ( It will have the access of the parameter function).
     chrome.tabs.sendMessage(tabId, {
       type: "NEW",
       videoId: urlParameters.get("v"),
